cabdaa6983b1e2d82fb1aaaa303c4fb77465edc3
LeeRaeIk <dlfdolr@daum.net>
2023-03-02 10:11:17 +0900

    portfolio2

--- js/main_swiper.js
+++ js/main_swiper.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('div.main div.swiper', { //프렘의 요소명
+const swiper = new Swiper('div.inner div.swiper', { //프렘의 요소명
   direction: 'horizontal', //스와이프의 방향
   loop: true, //순환여부
   // effect: 'slide',
@@ -13,17 +13,18 @@ const swiper = new Swiper('div.main div.swiper', { //프렘의 요소명
     nextEl: '.swiper-button-next', //다음 버튼 요소명
     prevEl: '.swiper-button-prev', //이전 버튼 요소명
   },
-
+  slidePerview : 'auto',
   grabCursor: true, //스와이프 시 마우스 포인터 모양 변경
   centeredSlides : true, // 슬라이더 패널을을 항상 가운데로 반환
   speed: 1500, //슬라이더 이동 속도 지정
   effect : 'slide', // 이팩트 효과
+  
 
   autoplay : { // 자동 슬라이드
     delay : 5000,
     disableOnInteraction : true
   },
-  slidesPerView : '1' // css에 지정한 슬라이더의 크리를 반영
+  
 });
 
 
--- js/model_swiper.js
+++ js/model_swiper.js
@@ -1,4 +1,4 @@
-const swiper2 = new Swiper('div.modelview div.swiper', {
+const swiper2 = new Swiper('div.model div.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
@@ -13,9 +13,9 @@ const swiper2 = new Swiper('div.modelview div.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  spaceBetween:500,
+
   grabCursor: true, //스와이프 시 마우스 포인터 모양 변경
-  slidesPerView: 'auto', // css에 지정한 슬라이더의 크리를 반영
+  slidesPerView: '3', // css에 지정한 슬라이더의 크리를 반영
   centeredSlides : true, // 슬라이더 패널을을 항상 가운데로 반환
   speed: 1500, //슬라이더 이동 속도 지정
   effect : 'slide', // 이팩트 효과
@@ -23,5 +23,17 @@ const swiper2 = new Swiper('div.modelview div.swiper', {
   scrollbar: {
     el: '.swiper-scrollbar',
   },
-});
+  breakpoints: {
+    1200:{
+      slidesPerView:3,
+    },
+    860: {
+      slidesPerView:2,
+    
+    },
+    460: {
+      slidesPerView: 1,
 
+    }
+  },
+});
